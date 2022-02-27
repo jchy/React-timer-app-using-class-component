fbc53a6717e4143f9746498437fb9a788bea77c8
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2022-02-27 22:22:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Timer from "./timer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>My Timer APP</h1>
+      <Timer />
+    </div>
+  );
+}
